886229acb9bdf12aa8bab37f11e6d84ea25228ee
Shivam <zeproffesor@gmail.com>
2020-04-14 03:43:54 +0530

    wapaas bc

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -24,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '96ec8rm7se#8-tea^n6u4t+cl=9q8q7x55&))85=9ojkk@ike@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -81,17 +80,15 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 #these lines connect our website to the postgresql database "portfoliodb" that we have created
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'portfoliodb_',
         'USER': 'postgres',
         'PASSWORD': 'Shivam@1101',
         'HOST': 'localhost',
-        'PORT': '',
+        'PORT': '5432',
     }
 }
 
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
